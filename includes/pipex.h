13cd06dbe1cfb51c098c4a3bfb41c577ce477795
Yassin MEHLIL <ymehlil@e1r3p7.clusters.42paris.fr>
2023-02-08 21:09:35 +0100

    transfert to gh

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ymehlil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 12:45:19 by ymehlil           #+#    #+#             */
-/*   Updated: 2023/01/26 12:48:34 by ymehlil          ###   ########.fr       */
+/*   Updated: 2023/02/08 17:44:55 by ymehlil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,17 @@
 # include "../get_next_line/get_next_line.h"
 # include "../libft/libft.h"
 # include "../ft_printf/ft_printf.h"
+# include <fcntl.h>
+
+// typedef struct s_pipex
+// {
+// 	int tube[2];
+// 	int f1;
+// 	int f2;
+// 	char *path;
+// 	char *cmd;
+// 	char cmd_paths;
+// }
 
 int	main();
 
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ymehlil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 12:48:46 by ymehlil           #+#    #+#             */
-/*   Updated: 2023/02/08 16:50:00 by ymehlil          ###   ########.fr       */
+/*   Updated: 2023/02/08 21:09:23 by ymehlil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,9 @@
 
 char	*take_path(char **env)
 {
-	int	i;
-
-	i = 0;
-	while (env[i])
-	{
-		if (env[i][0] == 'P' && env[i][1] == 'A' && env[i][2] == 'T')
-			return (env[i]);
-		i++;
-	}
-	return (NULL);
+	while (env && ft_strncmp("PATH", *env, 4))
+		env++;
+	return (*env);
 }
 
 char	*find_path(char *cmd, char *env)
@@ -48,17 +41,43 @@ char	*find_path(char *cmd, char *env)
 	
 }
 
+void child_process(int f1, char *cmd)
+{
+	dup2()
+}
+
+void	pipex(int f1, int f2, char *cmd1, char *cmd2)
+{
+	int	end[2];
+	pid_t	pid;
+	
+	pipe(end);
+	pid = fork();
+	if (parent < 0)
+		return(perror("Fork : "));
+	if (!parent)
+		child_process(f1, cmd1);
+	else
+		parent_process(f2, cmd2);
+}
+
 int	main(int ac, char **av, char **env)
 {
-	char *path;
-	char **cmd;
-	(void)ac;
+	char	*path;
+	char	**cmd;
+	int		f1;
+	int		f2;
 	
+	if (ac != 5)
+		return (ft_printf("Error\n: not the right amount of args"), 0);
 	path = take_path(env);
 	// AV[1] PROVISOIRE !!! A CHANGER
-	cmd = ft_split(av[1], ' ');
+	cmd = ft_split(av[2], ' ');
 	path = find_path(cmd[0], path);
-	ft_printf("%s\n", cmd[0]);
-	ft_printf("%s\n", path);
+	if (!path)
+		return (ft_printf("Error\nnot path found"), 0);
+	f1 = open(av[1], O_RDONLY);
+	f2 = open(av[ac - 1], O_TRUNC | O_RDONLY | O_CREAT);
+
 	execve(path, cmd, env);
 }
